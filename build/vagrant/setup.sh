14b4e3118d684e4185723f4829d11686f2670361
Juanma <juanmanuel.torres@guadaltech.es>
2017-10-06 09:28:53 +0200

    Add sudo to setup

--- build/vagrant/setup.sh
+++ build/vagrant/setup.sh
@@ -1,15 +1,23 @@
 #!/bin/bash -e
 
+user=$(whoami)
+ELASTICBOX_PATH="/var/elasticbox"
+
 # Install ElasticBox bootstrap
-apt-get -y update
-apt-get -y install python-pip curl
+sudo apt-get -y update
+sudo apt-get -y install python-pip curl virtualenv
+
+virtualenv .viertualenv
+
+source .viertualenv/bin/activate
 
 pip install --no-compile elasticbox-docker
 
 # Create code and log folder
-mkdir -p /var/log/elastickube && chown vagrant:vagrant /var/log/elastickube
-mkdir -p /opt/elastickube && chown -R vagrant:vagrant /opt/elastickube
+sudo mkdir -p /var/log/elastickube && sudo chown $user:$user /var/log/elastickube
+sudo mkdir -p /opt/elastickube && sudo chown -R $user:$user /opt/elastickube
 
+sudo mkdir -p /var/elasticbox
 
 sudo bash -- << \
 _____________EXECUTE_BOXES_____________
@@ -18,5 +26,8 @@ export DEBIAN_FRONTEND=noninteractive
 export ELASTICBOX_PATH=/opt/elastickube/build
 export ELASTICBOX_INSTANCE_PATH=${ELASTICBOX_PATH}
 
-elasticbox run --install --exit
+.viertualenv/bin/elasticbox run --install --exit
 _____________EXECUTE_BOXES_____________
+
+echo "Exit .viertualenv, you need activate"
+deactivate
